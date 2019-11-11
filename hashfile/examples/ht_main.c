361ffe34bfdf0d41bb5da9b40ccd05f0eb08e118
xizayu <grekoskall@gmail.com>
2019-11-11 10:50:20 +0200

    Modified a bit to test the functions of 'hash_file.c'

--- hashfile/examples/ht_main.c
+++ hashfile/examples/ht_main.c
@@ -82,20 +82,22 @@ int main() {
     memcpy(record.surname, surnames[r], strlen(surnames[r]) + 1);
     r = rand() % 10;
     memcpy(record.city, cities[r], strlen(cities[r]) + 1);
+    //printf("%s\n", record.city);
 
     CALL_OR_DIE(HT_InsertEntry(indexDesc, record));
   }
 
   printf("RUN PrintAllEntries\n");
   int id = rand() % RECORDS_NUM;
-  CALL_OR_DIE(HT_PrintAllEntries(indexDesc, &id));
-  //CALL_OR_DIE(HT_PrintAllEntries(indexDesc, NULL));
+  //CALL_OR_DIE(HT_PrintAllEntries(indexDesc, &id));
+  CALL_OR_DIE(HT_PrintAllEntries(indexDesc, NULL));
 
   printf("Delete Entry with id = %d\n" ,id);
   CALL_OR_DIE(HT_DeleteEntry(indexDesc, id));
   printf("Print Entry with id = %d\n", id); 
   CALL_OR_DIE(HT_PrintAllEntries(indexDesc, &id)); // must print something like : Entry doesn't exist or nothing at all
 
+  //CALL_OR_DIE(HT_PrintAllEntries(indexDesc, NULL));
   CALL_OR_DIE(HT_CloseFile(indexDesc));
   BF_Close();
 }
